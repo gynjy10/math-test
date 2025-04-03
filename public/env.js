58a36f1f6aece4a5169b5ba4eed62651d57abbeb
?? <gynjy10@gmail.com>
2025-04-03 20:01:57 +0900

    fix & fix

--- public/env.js
+++ public/env.js
@@ -1,4 +1,5 @@
 window.env = {
     FIREBASE_API_KEY: "AIzaSyCsZw0HYxfXIKIkS4lizDt-w5qTGUxQ3EM",
-    AUTH_DOMAIN: "mathproblemdb-9f42d.firebaseapp.com"
+    AUTH_DOMAIN: "mathproblemdb-9f42d.firebaseapp.com",
+    PROJECT_ID: "mathproblemdb-9f42d"
 }; 
