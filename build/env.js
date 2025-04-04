34e7df6bf93e7ec86fb2f126426372405657e7ea
?? <gynjy10@gmail.com>
2025-04-04 18:21:10 +0900

    logo

--- build/env.js
+++ build/env.js
@@ -1,5 +1,5 @@
 window.env = {
-    FIREBASE_API_KEY: "AIzaSyCsZw0HYxfXIKIkS4lizDt-w5qTGUxQ3EM",
+    FIREBASE_API_KEY: "AIzaSyCsZw0HYxfXIKIkS4lizDt...",
     AUTH_DOMAIN: "mathproblemdb-9f42d.firebaseapp.com",
     PROJECT_ID: "mathproblemdb-9f42d"
 }; 
